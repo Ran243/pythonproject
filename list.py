6f7cc5aca047a4e2c51ad313db540730142fc1d5
RanG <ryan.guoran@gmail.com>
2020-09-03 12:02:16 -0400

    Create list.py

---
+++ list.py
@@ -0,0 +1,25 @@
+numbers  = ["one", "two", "three"]
+print(numbers[1])
+#the first item in a list is 0
+numbers.append("four")
+#add item to end of array
+print(numbers)
+numbers.pop()
+#removes a item from array, depends on number in (), if no input removes last item
+print(numbers)
+numbers.insert (0, "zero")
+numbers.insert (4, "four")
+#inserts number needs to know location first
+print (numbers)
+numbers = ['zero', 0, 'one', 1, 'two', 2, 'three', 3, 'four', 4]
+#you can also use the numbers.insert() function
+print(numbers)
+#using a loop makes it easier to print each item of the array
+count = 0
+while count < len(numbers):
+    print(numbers(count))
+    count = count + 1
+numbers = [
+    "zero", "one", "two", "three"
+    ["0", "1", "2", '3']]
+print (numbers[2], [1])
