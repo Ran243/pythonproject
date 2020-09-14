410e749e93c6e925631279ee9ba3edfd8328226f
RanG <ryan.guoran@gmail.com>
2020-09-14 17:50:50 -0400

    trying out turtle

---
+++ turtleTest.py
@@ -0,0 +1,16 @@
+import turtle
+
+#Draw a square
+#for i in range(4):
+    #turtle.forward(200)
+    #turtle.left(90)
+
+#turtle.goto(100,100)
+#turtle.circle(60)
+#turtle.dot(60)
+#calling for the mainloop()
+turtle.shapesize(1,5,10)
+turtle.mainloop()
+
+
+#t.tr for right - t.fd for forward - t.lt for left - t.bk for backwad 
