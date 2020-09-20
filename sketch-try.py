3b722b9104714dfb5efc364d65f7c3fff528b693
RanG <ryan.guoran@gmail.com>
2020-09-20 19:54:42 -0400

    chapter 4

---
+++ sketch-try.py
@@ -0,0 +1,14 @@
+data = open('sketch.txt')
+
+for each_line in data:
+    try:
+        (role, line_spoken) = each_line.splite(':', 1)
+        print(role,end='')
+        print('said:', end='')
+        print(line_spoken, end='')
+    except:
+        pass
+
+data.close()
+
+
