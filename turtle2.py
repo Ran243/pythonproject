bfde71e738328f45eecf9b76a7ebf4ad3112abc0
RanG <ryan.guoran@gmail.com>
2020-09-23 18:37:32 -0400

    turtle create a circle

---
+++ turtle2.py
@@ -0,0 +1,13 @@
+import turtle
+#square 
+turtle.speed(1)
+turtle.begin_fill()
+turtle.fd(100)
+turtle.rt(90)
+turtle.fd(100)
+turtle.rt(90)
+turtle.fd(100)
+turtle.rt(90)
+turtle.fd(100)
+turtle.end_fill()
+turtle.mainloop()
